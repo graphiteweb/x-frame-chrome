6e846f1419148633a880b1db6d19131276876c25
Cbass92 <4h2rj3gk@duck.com>
2023-09-02 14:00:02 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
-    for (var i = 0; i < details.responseHeaders.length; i++) {
-      if (details.responseHeaders[i].name.toUpperCase() === "X-FRAME-OPTIONS") {
-        details.responseHeaders.splice(i, 1);
-        break;
-      }
-    }
-    return { responseHeaders: details.responseHeaders };
+    const newHeaders = details.responseHeaders.filter(header => {
+      return header.name.toUpperCase() !== "X-FRAME-OPTIONS";
+    });
+
+    return { responseHeaders: newHeaders };
   },
   { urls: ["<all_urls>"] },
   ["blocking", "responseHeaders"]
